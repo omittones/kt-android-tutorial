9f193403b63729c5fe0a3e6cc8501a1d9de07dbb
Augustin Juricic <augustin.juricic@gmail.com>
2020-11-08 17:09:50 +0100

    Add new activity

--- app/src/main/java/com/example/tutorial/MainActivity.kt
+++ app/src/main/java/com/example/tutorial/MainActivity.kt
@@ -1,27 +1,44 @@
 package com.example.tutorial
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import java.io.File
+
+const val EXTRA_MESSAGE = "some extra message"
 
 class MainActivity : AppCompatActivity() {
+
+    private val sendButton: Button
+    get() {
+        return this.findViewById(R.id.sendNotification)
+    }
+
+    private val notificationText: String
+    get() {
+        return this.findViewById<EditText>(R.id.notificationText).text.toString()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        val navView: BottomNavigationView = findViewById(R.id.nav_view)
-//
-//        val navController = findNavController(R.id.nav_host_fragment)
-//        // Passing each menu ID as a set of Ids because each
-//        // menu should be considered as top level destinations.
-//        val appBarConfiguration = AppBarConfiguration(setOf(
-//                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
-//        setupActionBarWithNavController(navController, appBarConfiguration)
-//        navView.setupWithNavController(navController)
+        this.sendButton.setOnClickListener(this::sendMessage)
+    }
+
+    private fun sendMessage(view: View) {
+        val message = this.notificationText
+        val intent = Intent(this, DisplayMessageActivity::class.java).apply {
+            putExtra(EXTRA_MESSAGE, message)
+        }
+        startActivity(intent)
     }
 }
