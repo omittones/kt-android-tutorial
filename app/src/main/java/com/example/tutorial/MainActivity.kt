b598e45e1dd03330ebe81e3e60301264d3895d39
Augustin Juricic <augustin.juricic@gmail.com>
2020-11-18 21:10:15 +0100

    Refresh text via timer.

--- app/src/main/java/com/example/tutorial/MainActivity.kt
+++ app/src/main/java/com/example/tutorial/MainActivity.kt
@@ -1,21 +1,31 @@
 package com.example.tutorial
 
+import android.annotation.SuppressLint
+import android.app.Activity
 import android.content.Intent
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
-import android.os.Bundle
-import android.os.Parcel
-import android.os.Parcelable
+import android.os.*
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.example.tutorial.keys.*
+import java.util.*
+import java.util.concurrent.TimeUnit
 
 class MainActivity() : AppCompatActivity(), SensorEventListener {
+
+    private var sensorValues: String = ""
+    private var timestamp: Long = 0
+    private lateinit var timer: Timer
+    private lateinit var manager: SensorManager
+    private lateinit var sensor: Sensor
 
     private val sendButton: Button
     get() {
@@ -27,45 +37,56 @@ class MainActivity() : AppCompatActivity(), SensorEventListener {
         return this.findViewById<EditText>(R.id.notificationText).text.toString()
     }
 
-    private var manager: SensorManager? = null
-    private var sensor: Sensor? = null
-
-    override fun onDestroy() {
-        super.onDestroy()
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         manager = this.getSystemService(SensorManager::class.java)
-        sensor = manager!!.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+        sensor = manager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
 
         this.sendButton.setOnClickListener(this::sendMessage)
     }
 
+    private fun startTimer() {
+        this.timer = Timer()
+        val activity = this
+        val logView = activity.findViewById<TextView>(R.id.logView)
+        val task = object : TimerTask() {
+            @SuppressLint("SetTextI18n")
+            override fun run() {
+                activity.runOnUiThread {
+                    logView.text = "${activity.timestamp} : ${activity.sensorValues}"
+                }
+            }
+        }
+        this.timer.schedule(task, 0, 100)
+    }
+
     override fun onStart() {
         super.onStart()
-        manager!!.registerListener(this, sensor, SensorManager.SENSOR_DELAY_FASTEST)
+        manager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_FASTEST)
+        startTimer()
     }
 
     override fun onResume() {
         super.onResume()
-        manager!!.registerListener(this, sensor, SensorManager.SENSOR_DELAY_FASTEST)
+        manager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_FASTEST)
+        startTimer()
     }
 
     override fun onPause() {
         super.onPause()
-        manager!!.unregisterListener(this)
+        manager.unregisterListener(this)
+        this.timer.cancel()
     }
 
     override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
     }
 
     override fun onSensorChanged(event: SensorEvent?) {
-        val representation = event!!.values.joinToString(transform = Float::toString)
-        val logView = this.findViewById<TextView>(R.id.logView)
-        logView.text = representation
+        this.sensorValues = event!!.values.joinToString(transform = Float::toString)
+        this.timestamp = event.timestamp
+        //Log.i(null, "${event.timestamp} : ${this.sensorValues}")
     }
 
     private fun sendMessage(view: View) {
